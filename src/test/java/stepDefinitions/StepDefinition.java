e068cc37db43c2cbff5588c55dee794861a7a198
Rajkamal <Rajkamal2win@gmail.com>
2021-12-12 03:07:11 +0530

    Onsite Updated

--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -13,12 +13,13 @@ public class StepDefinition {
 
     @Given("^User is on Netbanking landing page$")
     public void user_is_on_netbanking_landing_page() throws Throwable {
-    	System.out.println("GITDemo Ready");
+    	System.out.println("GITDemo");
     	System.out.println("A");
     }
 
     @When("^User login into application with username and password$")
     public void user_login_into_application_with_username_and_password() throws Throwable {
+    	System.out.println("GitTest");
     	System.out.println("B");   
     }
     
