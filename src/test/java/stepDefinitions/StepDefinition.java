aca05d16e35273c4e6d8ec7ac7f060585c08d53d
Rajkamal <Rajkamal2win@gmail.com>
2021-12-12 02:37:44 +0530

    first commit

---
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -0,0 +1,40 @@
+	package stepDefinitions;
+	
+	import org.junit.runner.RunWith;
+	
+	import io.cucumber.java.en.And;
+	import io.cucumber.java.en.Given;
+	import io.cucumber.java.en.Then;
+	import io.cucumber.java.en.When;
+	import io.cucumber.junit.Cucumber;
+	
+@RunWith(Cucumber.class)
+public class StepDefinition {
+
+    @Given("^User is on Netbanking landing page$")
+    public void user_is_on_netbanking_landing_page() throws Throwable {
+    	System.out.println("A");
+    }
+
+    @When("^User login into application with username and password$")
+    public void user_login_into_application_with_username_and_password() throws Throwable {
+    	System.out.println("B");   
+    }
+    
+    @When("User login into application with {string} and {string}")
+    public void user_login_into_application_with_and(String string, String string2) {
+        System.out.println(string);
+        System.out.println(string2);
+    }
+
+    @Then("^Home page is populated$")
+    public void home_page_is_populated() throws Throwable {
+    	System.out.println("C");
+    }
+
+    @And("^Cards are displayed$")
+    public void cards_are_displayed() throws Throwable {
+    	System.out.println("D");
+    }
+
+}
